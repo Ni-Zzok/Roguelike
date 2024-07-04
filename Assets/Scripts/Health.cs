21a5948bd450e36e8f5cdfac37a81be55f20d32a
Ni-Zzok <s.kostin.14@gmail.com>
2024-07-05 02:39:17 +0300

    Death Menu
    Меню при смерти с рестартом

--- Assets/Scripts/Health.cs
+++ Assets/Scripts/Health.cs
@@ -1,11 +1,36 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class Health : MonoBehaviour
 {
     public int health;
     public int maxHealth;
+    public GameObject pauseMenu;
+
+    void Start()
+    {
+        pauseMenu.SetActive(false);
+    }
+
+    public void PauseGame()
+    {
+        pauseMenu.SetActive(true);
+        Time.timeScale = 0f;
+    }
+
+    public void GoToMainMenu()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("MenuScene");
+    }
+
+    public void Restart()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+    }
 
     public void TakeHit(int damage)
     {
@@ -13,7 +38,7 @@ public class Health : MonoBehaviour
 
         if (health <= 0)
         {
-            Destroy(gameObject);
+            StopGame();
         }
     }
 
@@ -25,5 +50,11 @@ public class Health : MonoBehaviour
         {
             health = maxHealth;
         }
+    }
+
+    public void StopGame()
+    {
+        PauseGame();
+        //Destroy(gameObject);
     }
 }
