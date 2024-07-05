520c2dbebacb57a8f383d895c8dc2e5e3d8c218d
Alexandr <funktel1@gmail.com>
2024-07-06 00:00:06 +0300

    Должно работать
    устал(((((((

--- Assets/Scripts/RoomSpawner.cs
+++ Assets/Scripts/RoomSpawner.cs
@@ -14,6 +14,7 @@ public class RoomSpawner : MonoBehaviour
         Left,
         Right,
         None
+
     }
 
     private RoomVariants variants;
@@ -23,26 +24,21 @@ public class RoomSpawner : MonoBehaviour
 
     private void Start()
     {
-        Debug.Log("RoomSpawner started"); // Сообщение о старте скрипта
         variants = GameObject.FindGameObjectWithTag("Rooms").GetComponent<RoomVariants>();
         Destroy(gameObject, waitTime);
-        if (!spawned) // Проверяем перед вызовом Invoke
-        {
-            Debug.Log("Invoking Spawn method");
-            Invoke("Spawn", 0.2f);
-        }
-    }
+        Invoke("Spawn", 0.2f);
 
+    }
     public void Spawn()
     {
         if (!spawned)
         {
-            Debug.Log("Spawning room in direction: " + direction); // Сообщение о направлении спавна
             if (direction == Direction.Top)
             {
                 rand = Random.Range(0, variants.topRooms.Length);
                 Instantiate(variants.topRooms[rand], transform.position, variants.topRooms[rand].transform.rotation);
             }
+
             else if (direction == Direction.Bottom)
             {
                 rand = Random.Range(0, variants.bottomRooms.Length);
@@ -57,20 +53,24 @@ public class RoomSpawner : MonoBehaviour
             {
                 rand = Random.Range(0, variants.leftRooms.Length);
                 Instantiate(variants.leftRooms[rand], transform.position, variants.leftRooms[rand].transform.rotation);
+
             }
 
             spawned = true;
-            Debug.Log("Room spawned successfully"); // Сообщение о успешном спавне комнаты
         }
+
+
     }
 
-    private void OnTriggerEnter2D(Collider2D other)
+    private void OnTriggerStay2D(Collider2D other)
     {
-        Debug.Log("Triggered with: " + other.tag); // Сообщение о коллизии
         if (other.CompareTag("RoomPoint") && other.GetComponent<RoomSpawner>().spawned)
         {
-            Debug.Log("Destroying spawner gameObject"); // Сообщение об уничтожении объекта
             Destroy(gameObject);
+
         }
+
     }
+
+
 }
