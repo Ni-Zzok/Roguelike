e968578c163f120c5ae5673057d4cb182e8ae0e7
Ni-Zzok <s.kostin.14@gmail.com>
2024-07-12 02:44:20 +0300

    анимации противника (по диагонали вроде норм??)
    изменил скрипт передвижения (только для присвоения переменных в анимации) противника (старый скрипт можно найти в мистрале)

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,6 +13,9 @@ public class Enemy : MonoBehaviour
 
     private AddRoom room; // Ссылка на компонент AddRoom, чтобы управлять списком врагов
 
+    public Animator animator;
+    private bool facingRight = true;
+
     private void Start()
     {
         // Находим объект игрока на сцене
@@ -20,13 +23,16 @@ public class Enemy : MonoBehaviour
 
         // Получаем ссылку на родительский объект AddRoom
         room = GetComponentInParent<AddRoom>();
+
+        animator = GetComponent<Animator>();
     }
 
     private void Update()
     {
         if (health <= 0)
         {
-            Destroy(gameObject); // Уничтожаем врага, если его здоровье меньше или равно нулю
+            animator.SetTrigger("Dead");
+            Destroy(gameObject, 0.4f); // Уничтожаем врага, если его здоровье меньше или равно нулю
             room.enemies.Remove(gameObject); // Удаляем врага из списка текущих врагов в комнате
         }
         else
@@ -42,8 +48,44 @@ public class Enemy : MonoBehaviour
             // Вычисляем направление к игроку
             Vector2 direction = (player.position - transform.position).normalized;
 
+            if (direction.x < 0 && facingRight)
+            {
+                Flip();
+            }
+
+            if (direction.x > 0 && !facingRight)
+            {
+                Flip();
+            }
+
             // Перемещаем врага в этом направлении с учетом скорости
             transform.Translate(direction * speed * Time.deltaTime);
+
+            // Определяем, ближе к какому направлению двигается противник
+            float moveX = 0f;
+            float moveY = 0f;
+            if (Mathf.Abs(direction.x) > Mathf.Abs(direction.y))
+            {
+                // Движение по горизонтали
+                moveX = direction.x > 0f ? 1f : -1f;
+            }
+            else
+            {
+                // Движение по вертикали
+                moveY = direction.y > 0f ? 1f : -1f;
+            }
+            // Устанавливаем значения moveX и moveY для анимации
+            animator.SetFloat("MoveX", moveX);
+            animator.SetFloat("MoveY", moveY);
+
+            // Меняем направление спрайта врага
+            if (moveX > 0 && !facingRight || moveX < 0 && facingRight)
+            {
+                facingRight = !facingRight;
+                Vector3 theScale = transform.localScale;
+                theScale.x *= -1;
+                transform.localScale = theScale;
+            }
         }
     }
 
@@ -89,4 +131,12 @@ public class Enemy : MonoBehaviour
         }
     }
 
+    private void Flip()
+    {
+        facingRight = !facingRight;
+
+        Vector3 theScale = transform.localScale;
+        theScale.x *= -1;
+        transform.localScale = theScale;
+    }
 }
