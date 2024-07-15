8662199cab44d0b4a0b52678835b2edcd4b09c3d
Ni-Zzok <s.kostin.14@gmail.com>
2024-07-15 17:09:52 +0300

    Mushrom animations
    Анимашки гриба + почистил кой чего

--- Assets/Scripts/EnemyShooter.cs
+++ Assets/Scripts/EnemyShooter.cs
@@ -13,14 +13,19 @@ public class EnemyShooter : MonoBehaviour
     public float speed = 3f;
     private Vector2 direction = Vector2.right;
     public ContactFilter2D filter;
-    private Collider2D collider;
+    private new Collider2D collider;
     private AddRoom room;
     public int health;
+
+    public Animator animator;
+    private bool facingRight = true;
+    private Health playerHealth;
 
     void Start()
     {
         collider = GetComponent<Collider2D>();
         room = GetComponentInParent<AddRoom>();
+        animator = GetComponent<Animator>();
 
         // Ищем игрока в комнате и обновляем переменную player
         GameObject newPlayer = GameObject.FindGameObjectWithTag("Player");
@@ -37,8 +42,8 @@ public class EnemyShooter : MonoBehaviour
     {
         if (health <= 0)
         {
-            //animator.SetTrigger("Dead");
-            Destroy(gameObject, 0.4f); // Уничтожаем врага, если его здоровье меньше или равно нулю
+            animator.SetTrigger("Dead");
+            Destroy(gameObject, 0.2f); // Уничтожаем врага, если его здоровье меньше или равно нулю
             room.enemies.Remove(gameObject); // Удаляем врага из списка текущих врагов в комнате
         }
 
@@ -48,6 +53,11 @@ public class EnemyShooter : MonoBehaviour
         {
             player = newPlayer;
         }
+        playerHealth = player.GetComponent<Health>();
+        if (playerHealth.health <= 0)
+        {
+            animator.SetBool("PlayerDead", true);
+        }
 
         if (Time.time > nextFireTime)
         {
@@ -56,6 +66,24 @@ public class EnemyShooter : MonoBehaviour
         }
 
         transform.position = (Vector2)transform.position + direction * speed * Time.deltaTime;
+
+        if (direction.x >= 0 || direction.x < 0)
+        {
+            animator.SetBool("MoveX", true);
+        }
+        else
+        {
+            animator.SetBool("MoveX", false);
+        }
+        float moveX = 0f;
+        moveX = direction.x > 0f ? 1f : -1f;
+        if (moveX > 0 && !facingRight || moveX < 0 && facingRight)
+        {
+            facingRight = !facingRight;
+            Vector3 theScale = transform.localScale;
+            theScale.x *= -1;
+            transform.localScale = theScale;
+        }
     }
 
     void OnCollisionEnter2D(Collision2D col)
@@ -69,6 +97,7 @@ public class EnemyShooter : MonoBehaviour
 
     void Shoot()
     {
+        Invoke("ShootAnim", 0.5f);
         GameObject bullet = Instantiate(bulletPrefab, bulletSpawnPoint.position, Quaternion.identity);
         Bullet bulletScript = bullet.GetComponent<Bullet>();
 
@@ -86,5 +115,9 @@ public class EnemyShooter : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage; // Наносим урон врагу
+    }
+    void ShootAnim()
+    {
+        animator.SetTrigger("Shoot");
     }
 }
